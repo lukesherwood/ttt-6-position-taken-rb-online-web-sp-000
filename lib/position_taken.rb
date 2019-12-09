e9fe42e019fe0c0fcb7794bd617e13eb6a96c13a
Luke Sherwood <luke.sherwood55@gmail.com>
2019-12-09 17:30:31 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,13 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
+  
   if board[index] == " "
     false
   elsif bord[index] == "X" || "O"
     true
   end
+
 end
 
 #board[index] == " " || ""
