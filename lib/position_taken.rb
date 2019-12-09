d843f36de28214a2d670578da03908a25cee08db
Luke Sherwood <luke.sherwood55@gmail.com>
2019-12-09 17:30:53 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,9 @@
 
 def position_taken?(board, index)
   
-  if board[index] == " "
+  if board[index] == " " 
     false
-  elsif bord[index] == "X" || "O"
+  elsif board[index] == "X" || "O"
     true
   end
 
